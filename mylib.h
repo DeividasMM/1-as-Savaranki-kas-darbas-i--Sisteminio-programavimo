2f93d90a88698c305a16de162a39fecb10842821
Deividas Miliauskas <145470765+DeividasMM@users.noreply.github.com>
2024-10-31 09:30:43 +0200

    mylib.h

--- mylib.h
+++ mylib.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <array>
 #include <vector>
+#include <list>
+#include <deque>
 #include <iomanip>
 #include <algorithm>
 #include <cstdlib>
@@ -20,6 +22,8 @@ using std::cin;
 using std::endl;
 using std::array;
 using std::vector;
+using std::list;
+using std::deque;
 using std::setw;
 using std::left;
 using std::right;
@@ -38,6 +42,9 @@ using std::runtime_error;
 using std::to_string;
 using std::exception;
 using std::sort;
+using std::accumulate;
+using std::move;
+using namespace std::chrono;
 
 class Studentas {
     string vardas, pavarde;
@@ -59,24 +66,25 @@ public:
 
     void skaiciuotiGalutiniVidurki();
     void skaiciuotiGalutiniMediana();
-    double skaiciuotiMediana(vector<int> vec);
+    double skaiciuotiMediana(vector<int>& vec);
 
     inline string gautiVarda() const { return vardas; }
     inline string gautiPavarde() const { return pavarde; }
-    inline vector<int> gautiPazymius() const { return pazymiai; }
+    inline const vector<int>& gautiPazymius() const { return pazymiai; }
     inline int gautiEgzaminoBala() const { return egzaminas; }
     inline float gautiGalutiniBala() const { return galutinisBalas; }
     inline char gautiSkaiciavimoStrategija() const { return skaiciavimoStrategija; }
 
     inline int gautiPazymiuKieki() const { return pazymiai.size(); }
 
-    inline float gautiGalutiniBalaVidurkis() const { return galutinisBalas; }
-    inline float gautiGalutiniBalaMediana() const { return galutinisBalas; }
-
     inline void nustatytiSkaiciavimoStrategija(char strategija) { skaiciavimoStrategija = strategija; }
+
+    static Studentas ivestiNaujaStudenta();
 
     friend istream& operator>>(istream& input, Studentas& studentas);
     friend ostream& operator<<(ostream& output, const Studentas& studentas);
 };
+
+void sortContainer(list<Studentas>& container);
 
 #endif
